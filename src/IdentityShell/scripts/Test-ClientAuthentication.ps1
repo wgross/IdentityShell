66573e0e93f8c6d90bb5bcfc59ceec504918923a
Wolfgang Gross <github@text-mode.de>
2021-01-05 10:51:30 +0100

    [FEATURE] replaces netcoreapp3.1 with net5.0
    - [FIX]: Pester test for client authentication fails:
    -- Pester contexts seems to interfere with the setup of the test environment
    
    commit 6afd43204edd62594ea658eefba6a405cc8be852
    Author: Wolfgang Gross <github@text-mode.de>
    Date:   Mon Jan 4 17:07:17 2021 +0100
    
        target framework netcoreapp replaced with net5.0

--- src/IdentityShell/scripts/Test-ClientAuthentication.ps1
+++ src/IdentityShell/scripts/Test-ClientAuthentication.ps1
@@ -7,48 +7,18 @@ Describe "Protecting an API using client credentials" {
     BeforeAll {
         clean_configurationstore
         clean_aspidentitystore
-    }
-
-    Context "Configuration of the protected API" {
-   
         $apiScope = Set-IdentityApiScope -Name api1 
-        It "requires an api scope 'api1'" {
-            $apiScope|Should -Not -Be $null
-            $apiScope.Name | Should -Be "api1"
-        }
-
         $apiResource = Set-IdentityApiResource -Name api1 -DisplayName "My Api" -Scopes $apiScope.Name
-        It "defines an api resource" {
-            $apiResource|Should -Not -Be $null
-            $apiResource.Name | Should -Be "api1"
-            $apiResource.Scopes | Should -Be "api1"
-        }
-    }
-
-    Context "Configuration of the client acessing the API" {
-
         $secretHash = "secret"|sha256base64
-        $clientSecret = New-IdentitySecret -Value $secretHash
-        It "has a secret added to the client" {
-            $clientSecret.Value|Should -Be $secretHash
-        }
-
+        $clientSecret = New-IdentitySecret -Value $secretHash    
         $client = Set-IdentityClient -ClientId client -AllowedGrantTypes client_credentials -ClientSecrets $clientSecret -AllowedScopes "api1","openid"
-        It "rewqures a client with access to 'Api1'" {
-            $client.ClientSecrets.Length|Should -Be 1
-            $client.ClientSecrets[0].Value|Should -Be $secretHash
-            $client.AllowedScopes | Should -Be @("api1","openid")
-        }
     }
 
     Context "Requesting client access token" {
-
-        $discoveryDoc = Invoke-IdentityDiscoveryEndpoint         
-        $token = Invoke-IdentityTokenEndpoint -ClientId "client" -ClientSecret "secret" -Scopes "api1" -Endpoint $discoveryDoc.TokenEndpoint -TokenVariableName "tokenvar"
         It "retrieves an access token from IdentityServer" {
+            $discoveryDoc = Invoke-IdentityDiscoveryEndpoint                     
+            $token = Invoke-IdentityTokenEndpoint -ClientId "client" -ClientSecret "secret" -Scopes "api1" -Endpoint $discoveryDoc.TokenEndpoint -TokenVariableName "tokenvar"
             $token | Should -Not -Be $null
-        }
-        It "sets the given token varaible" {
             $tokenvar|Should -Be $token.AccessToken
         }
     }
@@ -63,14 +33,13 @@ Describe "Protecting an API using client credentials" {
     #     }
     # }
 
-    Context "Requesting user info" {
-
-        $discoveryDoc = Invoke-IdentityDiscoveryEndpoint 
-        $token = Invoke-IdentityTokenEndpoint -ClientId "client" -ClientSecret "secret" -Scopes "api1" -Endpoint $discoveryDoc.TokenEndpoint
-        $userInfo = Invoke-IdentityUserInfoEndpoint -Endpoint $discoveryDoc.UserInfoEndpoint -Token $token.AccessToken
-        It "IdentityServer provides a user info data" {
-            $userInfo.IsError|Should -Be $false
+    Context "Requesting user info doesn not work for client authentication" {
+        It "IdentityServer provides a user info data" {       
+            $discoveryDoc = Invoke-IdentityDiscoveryEndpoint 
+            $token = Invoke-IdentityTokenEndpoint -ClientId "client" -ClientSecret "secret" -Scopes "api1" -Endpoint $discoveryDoc.TokenEndpoint
+            $userInfo = Invoke-IdentityUserInfoEndpoint -Endpoint $discoveryDoc.UserInfoEndpoint -Token $token.AccessToken
             $userInfo | Should -Not -Be $null
+            $userInfo.IsError|Should -Be $true            
         }
     }
 }
